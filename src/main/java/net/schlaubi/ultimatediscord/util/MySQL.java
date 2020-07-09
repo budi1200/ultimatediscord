4a1b89c59f73bfbc664bdf55aad982be2c84711f
Alen Budimir <alenab22@gmail.com>
2020-07-09 22:47:03 +0200

    Change mysql implementation

--- src/main/java/net/schlaubi/ultimatediscord/util/MySQL.java
+++ src/main/java/net/schlaubi/ultimatediscord/util/MySQL.java
@@ -1,6 +1,7 @@
 package net.schlaubi.ultimatediscord.util;
 
 import net.schlaubi.ultimatediscord.spigot.Main;
+import okhttp3.ConnectionPool;
 import org.apache.commons.dbcp.ConnectionFactory;
 import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
@@ -15,10 +16,10 @@ import java.sql.*;
 
 public class MySQL {
 
-    private static Connection connection;
+    //private static Connection connection;
     private static GenericObjectPool gPool = null;
 
-    public static void connect(){
+    /*public static void connect(){
         FileConfiguration cfg = Main.getConfiguration();
         String host = cfg.getString("MySQL.host");
         Integer port = cfg.getInt("MySQL.port");
@@ -37,10 +38,10 @@ public class MySQL {
             Bukkit.getConsoleSender().sendMessage("§4§l[UltimateDiscord]MySQL connection failed");
             e.printStackTrace();
         }
-    }
+    }*/
 
     @SuppressWarnings("unused")
-    public DataSource setUpPool() throws Exception {
+    public static DataSource setUpPool() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
 
         FileConfiguration cfg = Main.getConfiguration();
@@ -68,12 +69,7 @@ public class MySQL {
         return gPool;
     }
 
-    // This Method Is Used To Print The Connection Pool Status
-    private void printDbStatus() {
-        System.out.println("Max.: " + getConnectionPool().getMaxActive() + "; Active: " + getConnectionPool().getNumActive() + "; Idle: " + getConnectionPool().getNumIdle());
-    }
-
-    private static boolean isConnected(){
+    /*private static boolean isConnected(){
         return connection != null;
     }
 
@@ -85,166 +81,315 @@ public class MySQL {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 
     public static void createDatabase()
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS ultimatediscord( `id` INT NOT NULL AUTO_INCREMENT , `uuid` TEXT NOT NULL , `discordid` TEXT, `previouslyLinked` INT NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("CREATE TABLE IF NOT EXISTS ultimatediscord( `id` INT NOT NULL AUTO_INCREMENT , `uuid` TEXT NOT NULL , `discordid` TEXT, `previouslyLinked` INT NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
             ps.execute();
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
+            }
         }
     }
 
     public static boolean isUserLinked(Player player)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid =?");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        boolean returnVal = false;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid =?");
             ps.setString(1, player.getName());
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             if(rs.next()){
-                return (!rs.getString("discordid").equals("null"));
-            }else{
-                return false;
+                returnVal = (!rs.getString("discordid").equals("null"));
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing ResultSet Object
+                if(rs != null) {
+                    rs.close();
+                }
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
             }
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return false;
+        return returnVal;
     }
 
     public static boolean isFirstLink(Player player){
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid =?");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        boolean returnVal = false;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid =?");
             ps.setString(1, player.getName());
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
+
             if(rs.next()){
-                return (rs.getInt("previouslyLinked") == 0);
+                returnVal = (rs.getInt("previouslyLinked") == 0);
             }else{
-                return true;
+                returnVal = true;
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing ResultSet Object
+                if(rs != null) {
+                    rs.close();
+                }
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
             }
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return false;
+        return returnVal;
     }
 
     public static void createUser(String player, String identity)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
+        Connection connObj = null;
+        PreparedStatement ps = null;
 
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO ultimatediscord(`uuid`,`discordid`, `previouslyLinked`) VALUES (?, ?, 1)");
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("INSERT INTO ultimatediscord(`uuid`,`discordid`, `previouslyLinked`) VALUES (?, ?, 1)");
             ps.setString(1, player);
             ps.setString(2, identity);
             ps.execute();
 
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
+            }
         }
     }
 
     public static void updateUser(String identity)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
+        Connection connObj = null;
+        PreparedStatement ps = null;
 
-            PreparedStatement ps = connection.prepareStatement("UPDATE ultimatediscord SET `discordid`=?, `previouslyLinked`=1");
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("UPDATE ultimatediscord SET `discordid`=?, `previouslyLinked`=1");
             ps.setString(1, identity);
             ps.execute();
 
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
+            }
         }
     }
 
 
     public static String getValue(Player player, String type)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid = ?");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String returnVal = null;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid = ?");
             ps.setString(1, player.getName());
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             if (rs.next()) {
-                return rs.getString(type);
+                returnVal = rs.getString(type);
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing ResultSet Object
+                if(rs != null) {
+                    rs.close();
+                }
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
             }
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return null;
+        return returnVal;
     }
 
     public static String getValue(String identity, String type)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE discordid = ?");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String returnVal = null;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("SELECT * FROM ultimatediscord WHERE discordid = ?");
             ps.setString(1, identity);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             if (rs.next()) {
-                return rs.getString(type);
+                returnVal = rs.getString(type);
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing ResultSet Object
+                if(rs != null) {
+                    rs.close();
+                }
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
             }
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return null;
+        return returnVal;
     }
 
     public static void unlinkUser(Player player)
     {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("UPDATE ultimatediscord SET `discordid`='null', `previouslyLinked`=1 WHERE uuid=?");
+        Connection connObj = null;
+        PreparedStatement ps = null;
+
+        try {
+            DataSource dataSource = setUpPool();
+
+            // Performing Database Operation!
+            System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+            connObj = dataSource.getConnection();
+
+            ps = connObj.prepareStatement("UPDATE ultimatediscord SET `discordid`='null', `previouslyLinked`=1 WHERE uuid=?");
             ps.setString(1, player.getName());
             ps.execute();
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
+
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Closing PreparedStatement Object
+                if(ps != null) {
+                    ps.close();
+                }
+                // Closing Connection Object
+                if(connObj != null) {
+                    connObj.close();
+                }
+            } catch(Exception sqlException) {
+                sqlException.printStackTrace();
+            }
         }
     }
-
 }
