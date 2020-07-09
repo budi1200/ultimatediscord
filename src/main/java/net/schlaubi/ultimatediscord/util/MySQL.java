84f4c36b8a7dcf9d550e956cbeffd2600c22d0b8
Alen Budimir <alenab22@gmail.com>
2020-07-09 21:31:28 +0200

    Update sql statements

--- src/main/java/net/schlaubi/ultimatediscord/util/MySQL.java
+++ src/main/java/net/schlaubi/ultimatediscord/util/MySQL.java
@@ -1,19 +1,22 @@
 package net.schlaubi.ultimatediscord.util;
 
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
 import net.schlaubi.ultimatediscord.spigot.Main;
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class MySQL {
 
     private static Connection connection;
+    private static GenericObjectPool gPool = null;
 
     public static void connect(){
         FileConfiguration cfg = Main.getConfiguration();
@@ -22,6 +25,7 @@ public class MySQL {
         String user = cfg.getString("MySQL.user");
         String database = cfg.getString("MySQL.database");
         String password = cfg.getString("MySQL.password");
+
 
         try
         {
@@ -35,21 +39,38 @@ public class MySQL {
         }
     }
 
-    public static void connect(Configuration config){
-        Configuration cfg = config;
+    @SuppressWarnings("unused")
+    public DataSource setUpPool() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        FileConfiguration cfg = Main.getConfiguration();
         String host = cfg.getString("MySQL.host");
-        Integer port = cfg.getInt("MySQL.port");
+        int port = cfg.getInt("MySQL.port");
         String user = cfg.getString("MySQL.user");
         String database = cfg.getString("MySQL.database");
         String password = cfg.getString("MySQL.password");
 
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true&autoReconnectForPools=true&interactiveClient=true&characterEncoding=UTF-8", user, password);
-            ProxyServer.getInstance().getConsole().sendMessage("§a§l[UltimateDiscord]MySQL connection success");
-        } catch (SQLException e) {
-            ProxyServer.getInstance().getConsole().sendMessage("§4§l[UltimateDiscord]MySQL connection failed");
-            e.printStackTrace();
-        }
+        String dbUrl = "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true&autoReconnectForPools=true&interactiveClient=true&characterEncoding=UTF-8";
+
+        // Creates an Instance of GenericObjectPool That Holds Our Pool of Connections Object!
+        gPool = new GenericObjectPool();
+        gPool.setMaxActive(5);
+
+        // Creates a ConnectionFactory Object Which Will Be Use by the Pool to Create the Connection Object!
+        ConnectionFactory cf = new DriverManagerConnectionFactory(dbUrl, user, password);
+
+        // Creates a PoolableConnectionFactory That Will Wraps the Connection Object Created by the ConnectionFactory to Add Object Pooling Functionality!
+        PoolableConnectionFactory pcf = new PoolableConnectionFactory(cf, gPool, null, null, false, true);
+        return new PoolingDataSource(gPool);
+    }
+
+    public GenericObjectPool getConnectionPool() {
+        return gPool;
+    }
+
+    // This Method Is Used To Print The Connection Pool Status
+    private void printDbStatus() {
+        System.out.println("Max.: " + getConnectionPool().getMaxActive() + "; Active: " + getConnectionPool().getNumActive() + "; Idle: " + getConnectionPool().getNumIdle());
     }
 
     private static boolean isConnected(){
@@ -105,8 +126,7 @@ public class MySQL {
         return false;
     }
 
-    public static boolean isUserLinked(ProxiedPlayer player)
-    {
+    public static boolean isFirstLink(Player player){
         try
         {
             if (connection.isClosed()) {
@@ -115,28 +135,11 @@ public class MySQL {
             PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid =?");
             ps.setString(1, player.getName());
             ResultSet rs = ps.executeQuery();
-            return rs.next();
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static boolean isUserLinked(String id)
-    {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
+            if(rs.next()){
+                return (rs.getInt("previouslyLinked") == 0);
+            }else{
+                return true;
             }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE discordid =?");
-            if (!Bukkit.getServer().getOnlineMode()) {
-                ps.setString(1, id);
-            }
-            ResultSet rs = ps.executeQuery();
-            return rs.next();
         }
         catch (SQLException e)
         {
@@ -153,20 +156,30 @@ public class MySQL {
                 connect();
             }
 
-            PreparedStatement ps1 = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE `uuid`=?");
-            ps1.setString(1, player);
-            ResultSet rs = ps1.executeQuery();
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO ultimatediscord(`uuid`,`discordid`, `previouslyLinked`) VALUES (?, ?, 1)");
+            ps.setString(1, player);
+            ps.setString(2, identity);
+            ps.execute();
 
-            if(rs.next()){
-                PreparedStatement ps = connection.prepareStatement("UPDATE ultimatediscord SET `discordid`=?, `previouslyLinked`=1");
-                ps.setString(1, identity);
-                ps.execute();
-            }else{
-                PreparedStatement ps = connection.prepareStatement("INSERT INTO ultimatediscord(`uuid`,`discordid`, `previouslyLinked`) VALUES (?, ?, 1)");
-                ps.setString(1, player);
-                ps.setString(2, identity);
-                ps.execute();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateUser(String identity)
+    {
+        try
+        {
+            if (connection.isClosed()) {
+                connect();
             }
+
+            PreparedStatement ps = connection.prepareStatement("UPDATE ultimatediscord SET `discordid`=?, `previouslyLinked`=1");
+            ps.setString(1, identity);
+            ps.execute();
+
         }
         catch (SQLException e)
         {
@@ -176,27 +189,6 @@ public class MySQL {
 
 
     public static String getValue(Player player, String type)
-    {
-        try
-        {
-            if (connection.isClosed()) {
-                connect();
-            }
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ultimatediscord WHERE uuid = ?");
-            ps.setString(1, player.getName());
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return rs.getString(type);
-            }
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getValue(ProxiedPlayer player, String type)
     {
         try
         {
@@ -254,8 +246,5 @@ public class MySQL {
             e.printStackTrace();
         }
     }
-
-
-
 
 }
